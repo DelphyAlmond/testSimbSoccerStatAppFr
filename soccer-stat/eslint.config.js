fc6737972fa32df9d4779df3fc234f5276c2184d
Delphy Almond <delphyalmond@gmail.com>
2026-03-14 20:45:17 +0400

    1 step : eslint+prettier settings

--- soccer-stat/eslint.config.js
+++ soccer-stat/eslint.config.js
@@ -5,19 +5,53 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
+/* + Prettier */
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
 export default defineConfig([
-  globalIgnores(['dist']),
+  // 1. glob.ignored.folders
+  { ignores: ['dist', 'node_modules', 'build'] },
+  // 2. base.conf.-s (JS + TS) (ко всем по дэфолту:)
+  js.configs.recommended,
+  ...tseslint.configs.recommended, // got [] - дост.список
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+    // 3. main.configuration + spec. for React + TS
+    // [х] extends: [ js.configs.recommended,
+    // ...tseslint.configs.recommended,
+    // (...)
+    // prettierConfig, (откл. конфликт.-ие rules)],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.es2020,
+      },
+      // [ ! ] TS: парсер, если tseslint не подхватил сам
+      parser: tseslint.parser,
+      parserOptions: {
+        project: ['./tsconfig.app.json', './tsconfig.node.json'],
+      }
+    },
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'prettier': prettierPlugin // + сам плагин Prettier
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true }
+      ],
+      // + Pr.-r
+      'prettier/prettier': 'error',
+      // актив.: пометка ошибки форматирования
+      // как ошибки линтинга
+      'no-console': 'warn' // prod-n*
     },
   },
-])
+  // 4. switch-off.conflict.rules
+  prettierConfig
+]);
